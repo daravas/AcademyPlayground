ddf2ab6ea45cf99def7542f995c1039bb3d6fee5
Dara Vasconcelos <dcsv@cin.ufpe.br>
2020-04-02 17:52:10 -0300

    interacao entre telas da introducao

--- AcademyPlayground.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ AcademyPlayground.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -1,5 +1,5 @@
 //: A UIKit based Playground for presenting user interface
-  
+
 import UIKit
 import PlaygroundSupport
 
@@ -7,29 +7,104 @@ import PlaygroundSupport
 let cfURL = Bundle.main.url(forResource: "SF-Pro-Text-Regular", withExtension: "otf")! as CFURL
 CTFontManagerRegisterFontsForURL(cfURL, CTFontManagerScope.process, nil)
 
-//cria cor de fundo
-//let backgroundImage =  UIImage(imageLiteralResourceName: "background.png")
-//et backgroundColor = UIColor(patternImage: backgroundImage) cria cor a partir de uma imagem
-//let background = UIImageView(image: backgroundImage)
 
 class MyViewController : UIViewController {
     
     override func loadView() {
         let view = UIView()
         view.backgroundColor = .roxoBackground
+        let textViewText = " O playground Nome te mostra as descobertas e histórias das mulheres da ciência, tecnologia,engenharia e matemática"
+        let textView = createTextView(text: textViewText)
         
-        let textView = createTextView()
-        
-        view.addSubview(textView)
         
         let btnNext = createButtonNext()
+        btnNext.addTarget(nil, action: #selector(tapBtnNext), for: .touchUpInside)
+        
+        view.addSubview(textView)
         view.addSubview(btnNext)
         
         //view.addSubview(viewBook)
         self.view = view
     }
     
-   
+    @objc func tapBtnNext() {
+        //show(secondViewController, sender: nil)
+     //   present(mySecondViewController, animated: true, completion: nil)
+        navigationController?.pushViewController(mySecondViewController, animated: true)
+    }
+    
+    
+}
+
+class MySecondViewController : UIViewController{
+    override func loadView() {
+        let view = UIView()
+        view.backgroundColor = .roxoBackground
+        let textViewText = " Encontre os objetos clicáveis dos cenários e  descubra mais sobre as mulheres, suas invenções e descobertas"
+        let textView = createTextView(text: textViewText)
+        
+        view.addSubview(textView)
+        
+        let btnPreview = createButtonPreview()
+        btnPreview.addTarget(nil, action: #selector(tapBtnPreview), for: .touchUpInside)
+        
+        view.addSubview(btnPreview)
+        
+        let btnNext = createButtonNext()
+        btnNext.addTarget(nil, action: #selector(tapBtnNext), for: .touchUpInside)
+        view.addSubview(btnNext)
+        
+        //view.addSubview(viewBook)
+        self.view = view
+    }
+    
+    @objc func tapBtnPreview() {
+        navigationController?.popViewController(animated: true)
+        
+    }
+    
+    @objc func tapBtnNext() {
+        navigationController?.pushViewController(myThirdViewController, animated: true)
+    }
+}
+
+class MyThirdViewController : UIViewController{
+    
+    override func loadView() {
+        let view = UIView()
+        view.backgroundColor = .roxoBackground
+        let textViewText = " Você pode trocar de cenário encontrando todos os objetos de uma cena ou na opção Trocar cenário - são 2 ambientes: laboratório e oficina"
+        
+        let textView = createTextView(text: textViewText)
+        
+        view.addSubview(textView)
+        
+        let btnPreview = createButtonPreview()
+        btnPreview.addTarget(nil, action: #selector(tapBtnPreview), for: .touchUpInside)
+        
+        view.addSubview(btnPreview)
+        
+        let btnNext = createButtonNext()
+        
+        view.addSubview(btnNext)
+        
+        //view.addSubview(viewBook)
+        self.view = view
+    }
+    
+    @objc func tapBtnPreview() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+}
+
+func createButtonPreview() -> UIButton{
+    let btnPreview = UIButton()
+    let image = UIImage(named: "btnPreview.png")
+    
+    btnPreview.frame = CGRect(x: 74, y: 425, width: 33, height: 52)
+    btnPreview.setImage(image, for: .normal)
+    return btnPreview
 }
 
 func createButtonNext() -> UIButton{
@@ -40,10 +115,10 @@ func createButtonNext() -> UIButton{
     return btnNext
 }
 
-func createTextView() -> UITextView{
+func createTextView(text:String) -> UITextView{
     let textView = UITextView()
     textView.frame = CGRect(x: 455, y: 364, width: 530, height: 200)
-    textView.text = " O playground Nome te mostra as descobertas e histórias das mulheres da ciência, tecnologia,engenharia e matemática"
+    textView.text = text
     textView.textAlignment = .center
     textView.font = UIFont(name: "SF Pro text", size: 32)
     textView.textColor = .white
@@ -52,5 +127,14 @@ func createTextView() -> UITextView{
     return textView
 }
 
-let vc = MyViewController(screenType: .mac, isPortrait: true)
-PlaygroundPage.current.liveView = vc.scale(to: 0.4)
+let myViewController = MyViewController()
+let mySecondViewController = MySecondViewController()
+let myThirdViewController = MyThirdViewController()
+
+let navigation = UINavigationController(screenType: .mac, isPortrait: true)
+navigation.navigationBar.isHidden = true
+navigation.pushViewController(myViewController, animated: true)
+PlaygroundPage.current.liveView = navigation.scale(to: 0.5)
+
+//let vc = MyViewController(screenType: .mac, isPortrait: true)
+//PlaygroundPage.current.liveView = vc.scale(to: 0.4)
